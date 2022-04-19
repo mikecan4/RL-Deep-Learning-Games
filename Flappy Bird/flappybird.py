073cbd0433737e445b41ef617c9c9b66d9a22239
Isaac Valdez <isaac.valdez01@utrgv.edu>
2022-04-18 23:39:02 -0500

    Added a comment to __name__ about how the neural network can make the perfect bird by chance with 100 birds

--- Flappy Bird/flappybird.py
+++ Flappy Bird/flappybird.py
@@ -285,3 +285,8 @@ if __name__ == "__main__":
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, "config-feedforward.txt")
     run(config_path)
+
+    # NOTE: because of how simple flappy bird is
+    # the game can get a perfect bird within 100 birds
+    # since the neural network needed to beat flappy bird
+    # is very simple!
